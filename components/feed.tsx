5b56f1509cc3d074907189bfa5c1567012a43566
mridul.kumar <mridul.kumar@procol.in>
2022-05-23 13:07:00 +0530

    added tweet box

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,5 +1,6 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
+import TweetBox from './tweetBox'
 
 function Feed() {
   return (
@@ -8,6 +9,7 @@ function Feed() {
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
         <RefreshIcon className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
       </div>
+      <TweetBox />
     </div>
   )
 }
